e7548118cd292c5b845836ac6a28e89ecb1b0a24
vipulchandan <plenteousvipul90009@gmail.com>
2023-07-24 12:02:17 +0530

    youtube transcript

---
+++ client/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import VideoForm from './components/VideoForm';
+import VideoPlayer from './components/VideoPlayer';
+
+
+const App = () => {
+  const [videoData, setVideoData] = useState(null);
+
+  return (
+    <div className="container">
+      <h1>YouTube Video Transcript Viewer</h1>
+      <VideoForm setVideoData={setVideoData} />
+      {videoData && (
+        <div>
+          <div className="video-container">
+            <VideoPlayer videoUrl={videoData.videoUrl} transcript={videoData.transcript} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+// const App = () => {
+//   const [videoData, setVideoData] = useState(null);
+
+//   return (
+//     <div className="container">
+//       <h1>YouTube Video Subtitle Viewer</h1>
+//       <VideoForm setVideoData={setVideoData} />
+//       {videoData && (
+//         <VideoPlayer videoUrl={videoData.videoUrl} transcript={videoData.transcript} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default App;

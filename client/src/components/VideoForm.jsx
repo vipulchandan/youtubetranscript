2879c1a43e36091db7ff4087e5984db186b7c399
vipulchandan <plenteousvipul90009@gmail.com>
2023-07-24 18:31:23 +0530

    Fixed link related bugs

--- client/src/components/VideoForm.jsx
+++ client/src/components/VideoForm.jsx
@@ -4,31 +4,45 @@ import axios from 'axios';
 const VideoForm = ({ setVideoData }) => {
   const [inputUrl, setInputUrl] = useState('');
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false); // Add the loading state
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError(''); 
-
+    setError('');
+    
     if (!inputUrl.trim()) {
       setError('Please enter a YouTube video URL.');
       return;
     }
-
+    
     const urlRegex = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=([^&]+).*/;
     if (!urlRegex.test(inputUrl)) {
       setError('Please enter a valid YouTube video URL.');
       return;
     }
+    
+    setIsLoading(true); // Set loading state to true
 
     try {
-      const response = await axios.post('http://localhost:5000/api/transcript', { videoUrl: inputUrl });
-      setVideoData(response.data);
-      if (!response.data.transcript) {
-        setError('Transcript not available for the entered YouTube URL.');
+      const response = await axios.post('http://localhost:5000/api/transcript', {
+        videoUrl: inputUrl,
+      });
+
+      const { videoUrl, transcript } = response.data;
+
+      if (transcript === null) {
+        setVideoData({
+          videoUrl,
+          transcript: 'Transcript not available for this video.',
+        });
+      } else {
+        setVideoData(response.data);
       }
     } catch (error) {
       console.log(error);
       setError('An error occurred while fetching the transcript.');
+    } finally {
+      setIsLoading(false); // Set loading state to false after the request is completed
     }
   };
 
@@ -40,13 +54,12 @@ const VideoForm = ({ setVideoData }) => {
         onChange={(e) => setInputUrl(e.target.value)}
         placeholder="Enter YouTube Video URL"
       />
-      <button type="submit">Submit</button>
+      <button type="submit" disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Submit'}
+      </button>
       {error && <p className="error">{error}</p>}
     </form>
   );
 };
 
 export default VideoForm;
-
-
-

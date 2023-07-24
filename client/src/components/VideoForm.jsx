7dfe258694935d95a4797798543e24d2a0f82395
vipulchandan <plenteousvipul90009@gmail.com>
2023-07-24 18:59:52 +0530

    fixing bugs for showing invalid links

--- client/src/components/VideoForm.jsx
+++ client/src/components/VideoForm.jsx
@@ -4,30 +4,28 @@ import axios from 'axios';
 const VideoForm = ({ setVideoData }) => {
   const [inputUrl, setInputUrl] = useState('');
   const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState(false); // Add the loading state
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
-    
+    setError(''); 
+
     if (!inputUrl.trim()) {
       setError('Please enter a YouTube video URL.');
       return;
     }
-    
+
     const urlRegex = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=([^&]+).*/;
     if (!urlRegex.test(inputUrl)) {
       setError('Please enter a valid YouTube video URL.');
       return;
     }
-    
-    setIsLoading(true); // Set loading state to true
+
+    setIsLoading(true);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/transcript', {
-        videoUrl: inputUrl,
-      });
-
+      // const response = await axios.post('https://creative-pebble-wilderness.glitch.me/api/transcript', { videoUrl: inputUrl });
+      const response = await axios.post('http://localhost:5000/api/transcript', { videoUrl: inputUrl });
       const { videoUrl, transcript } = response.data;
 
       if (transcript === null) {
@@ -38,11 +36,16 @@ const VideoForm = ({ setVideoData }) => {
       } else {
         setVideoData(response.data);
       }
+    
+      // setVideoData(response.data);
+      if (!response.data.transcript) {
+        setError('Transcript not available for the entered YouTube URL.');
+      }
     } catch (error) {
       console.log(error);
       setError('An error occurred while fetching the transcript.');
     } finally {
-      setIsLoading(false); // Set loading state to false after the request is completed
+      setIsLoading(false); 
     }
   };
 
@@ -63,3 +66,6 @@ const VideoForm = ({ setVideoData }) => {
 };
 
 export default VideoForm;
+
+
+
